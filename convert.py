e500ae1a079251b0541e0a0a94fc135c30c6f984
sergio apreda <sergioapreda@sergios-MacBook-Pro.local>
2019-01-16 16:20:47 +0000

    added group support

--- convert.py
+++ convert.py
@@ -1,25 +1,37 @@
-import math
-
 import numpy as np
 import zarr
 from netCDF4 import Dataset
-from concurrency import threaded
-from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+from itertools import chain
 from concurrent.futures import ProcessPoolExecutor
 
 # Convert NetCDF files to Zarr store
-def netcdf_to_zarr(datasets, store, append_axis):
-    root = zarr.group(store=store, overwrite = True)
-    for i, ds in enumerate(datasets):
-        if i == 0:
-            __set_meta(ds,root)
-            __set_dims(ds,root)
-            __set_vars(ds,root)
-        else:
-            __append_vars(ds, root, append_axis)
+
+
+def netcdf_to_zarr(datasets, store, append_axis=None):
+
+    root = zarr.group(store=store, overwrite=True)
+
+    for i, dname in enumerate(datasets):
+        for j, gname in enumerate(__get_groups(dname)):
+            if j == 0:
+                group = root
+                ds = ''
+            else:
+                group = __set_group(gname, root)
+                ds = dname
+            if i == 0:
+                __set_meta(ds + gname, group)
+                __set_vars_serial(ds + gname, group)
+            else:
+                __append_vars_serial(gname, group, append_axis)
+
 
 # Convert non-json-encodable types to built-in types
+
+
 def __json_encode(val):
+
     if isinstance(val, np.integer):
         return int(val)
     elif isinstance(val, np.floating):
@@ -29,90 +41,156 @@ def __json_encode(val):
     else:
         return val
 
-# Return attributes as dict
-def __dsattrs(dataset):
-        # JSON encode attributes so they can be serialized
-        return {key: __json_encode(getattr(dataset, key)) for key in dataset.ncattrs() }
 
-def __get_dataset(ds):
-    return Dataset(ds)
+# Open netcdf files and groups with the same interface
+
+
+def __nc_open(ds, *args, **kwargs):
+
+    tok = Path(ds).parts
+
+    if len(tok) < 2:
+        return Dataset(tok[0], *args, **kwargs)
+    else:
+        return Dataset(tok[0], *args, **kwargs)['/'.join(tok[1:])]
+
+
+# Return serielizable attributes as dicts
+
+
+def __dsattrs(dataset):
+
+    # JSON encode attributes so they can be serialized
+    return {key: __json_encode(getattr(dataset, key)) for key in dataset.ncattrs()}
+
+
+# Return chunking informations about the given variable
+
+
+def __get_var_chunks(var):
+
+    if var.chunking() != 'contiguous':
+        return tuple(var.chunking())
+
+    return None
+
+
+# Returns an iterator with every group of the file (root included)
+
+
+def __get_groups(ds):
+
+    # recursive version with generator
+    def walktree(top):
+        values = top.groups.values()
+        if values:
+            for v in values:
+                yield v.path
+
+        for value in values:
+            for children in walktree(value):
+                yield children
+
+    dataset = Dataset(ds)
+
+    grps = (g for g in walktree(dataset))
+
+    return chain((ds,), grps)
+
+
+# Set file group
+
+
+def __set_group(ds, store):
+
+    print('creating group: ' + ds)
+    return store.create_group(ds)
+
 
 # Set file metadata
-def __set_meta(ds, group):
-    print("Set meta")
-    group.attrs.put(__dsattrs(Dataset(ds)));
-
-def __set_dim(ds, group, name):
-    print("Set dim")
-    dataset = Dataset(ds)
-    dim = dataset.dimensions[name]
-    group.create_dataset(name, \
-        data=np.arange(dim.size), \
-        shape=(dim.size,), \
-        chunks=(1<<16,) if dim.isunlimited() else (dim.size,), \
-        dtype=np.int32 \
-    )
-    # Set dimension attrs
-    group[name].attrs['_ARRAY_DIMENSIONS'] = [name]
-
-# Set dimensions
-def __set_dims(ds, group):
-    dataset = Dataset(ds)
-    with ProcessPoolExecutor(max_workers=8) as executor:
-        for name, dim in dataset.dimensions.items():
-            executor.submit(__set_dim, ds, group, name)
 
 
-# Calculate chunk size for variable
-def __get_var_chunks(var, max_size):
-    chunks = []
-    # TODO: Improve chunk size calculation
-    for i, dim in enumerate(var.shape):
-        dim_chunk_length = min(math.floor(max_size ** (1/(len(var.shape)-i))), dim)
-        max_size //= dim_chunk_length
-        chunks.append(dim_chunk_length)
+def __set_meta(ds, store):
 
-    return tuple(chunks)
+    print("Setting meta for: " + ds)
+    store.attrs.put(__dsattrs(__nc_open(ds)))
+
 
 # Set variable data, including dimensions and metadata
 
-def __set_var(ds, group, name):
-    print("Setting " + name)
-    dataset = Dataset(ds)
-    var = dataset.variables[name]
-    group.create_dataset(name, \
-        data=var, \
-        shape=var.shape, \
-        chunks=(__get_var_chunks(var, 2<<24)), \
-        dtype=var.dtype \
-    )
-    attrs = __dsattrs(var)
-    attrs['_ARRAY_DIMENSIONS'] = list(var.dimensions)
-    group[name].attrs.put(attrs);
 
-def __set_vars(ds, group):
-    dataset = Dataset(ds)
-    print("Set vars")
-    with ProcessPoolExecutor(max_workers=8) as executor:
-        for name, var in dataset.variables.items():
-            executor.submit(__set_var, ds, group, name)
+def __set_var(ds, store, name):
+
+    print("Setting variable " + name)
+    dataset = __nc_open(ds)
+    var = dataset.variables[name]
+    store.create_dataset(name,
+                         data=var,
+                         shape=var.shape,
+                         chunks=(__get_var_chunks(var)),
+                         dtype=var.dtype
+                         )
+    attrs = __dsattrs(var)
+    attrs['dimensions'] = list(var.dimensions)
+    store[name].attrs.put(attrs)
 
 
 # Append data to existing variable
 
-def __append_var(ds, group, name, dim_name):
-    print("Appending " + name)
-    dataset = Dataset(ds)
-    var = dataset.variables[name]
-    if dim_name in var.dimensions:
-        axis = group[name].attrs['_ARRAY_DIMENSIONS'].index(dim_name)
-        group[name].append(var, axis)
 
-def __append_vars(ds, group, dim):
+def __append_var(ds, store, name, dim):
+    print("Appending " + name + " from " + ds)
+    dataset = __nc_open(ds)
+    var = dataset.variables[name]
+    print('dim: ' + dim, 'vdim: ' + var.dimensions)
+    if dim in var.dimensions:
+        axis = group[name].attrs['dimensions'].index(dim)
+        store[name].append(var, axis)
+
+
+# NOTE: zarr parallel writes fails  on High Sierra 10.13.6
+
+
+# serial execution
+
+
+def __set_vars_serial(ds, store):
+
+    print("Setting variables for: " + ds)
+    dataset = __nc_open(ds)
+
+    for name in dataset.variables.keys():
+        __set_var(ds, store, name)
+
+
+def __append_vars_serial(ds, store, dim):
+
     print("Append vars")
-    dataset = Dataset(ds)
-    group[dim].append(np.arange(group[dim].size, group[dim].size + dataset.dimensions[dim].size))
+    dataset = __nc_open(ds)
+
+    store[dim].append(dataset[dim])
+
+    for name in dataset.variables.keys():
+        __append_var(ds, store, name, dim)
+
+
+# parallel execution
+
+
+def __set_vars_parallel(ds, store):
 
     with ProcessPoolExecutor(max_workers=8) as executor:
-        for name, var in dataset.variables.items():
-            executor.submit(__append_var, ds, group, name, dim)
+        for name in dataset.variables.keys():
+            executor.submit(__set_var, ds, store, name)
+
+
+def __append_vars_parallel(ds, store, dim):
+
+    print("Append vars")
+    dataset = __nc_open(ds)
+
+    store[dim].append(dataset[dim])
+
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        for name in dataset.variables.keys():
+            executor.submit(__append_var, ds, store, name, dim)
