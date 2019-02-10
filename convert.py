b1261ca3d93a35f9cca60233bdcbb73039588f79
serapred <serapred@gmail.com>
2019-02-10 17:51:50 +0000

    bug fix

--- convert.py
+++ convert.py
@@ -12,18 +12,29 @@ from concurrent.futures import ThreadPoolExecutor
 SHARED = '/dev/shm/'
 
 
-# Convert NetCDF files to Zarr store
-# N.B. To use threads or processes
-# a thread safe and process safe version of hdf5 (underlying of netcdf4) is required.
-# this is because even if only concurrent reads on different data are issued,
-# the HDF5 library modifies global data structures that are independent
-# of a particular HDF5 dataset or HDF5 file.
-# HDF5 relies on a semaphore around the library API calls in the thread-safe version of the library
-# to protect the data structure from corruption by simultaneous manipulation from different threads.
+"""
+Convert NetCDF files to Zarr store
+N.B. To use threads or processes a thread/process safe version
+of hdf5 (underlying netcdf4) is required.
+this is because even if only concurrent reads on different data are issued,
+ the HDF5 library modifies global data structures that are independent
+ of a particular HDF5 dataset or HDF5 file.
+ HDF5 relies on a semaphore around the library API calls in the thread-safe
+ version of the library to protect the data structure from corruption
+ by simultaneous manipulation from different threads.
+"""
 
 
 def netcdf_to_zarr(src, dst, axis=None, mode='serial', nested=False):
+    """Summary
 
+    Args:
+        src (TYPE): Description
+        dst (TYPE): Description
+        axis (None, optional): Description
+        mode (str, optional): Description
+        nested (bool, optional): Description
+    """
     if isinstance(dst, str):
         if nested:
             local_store = zarr.NestedDirectoryStore(dst)
@@ -257,11 +268,11 @@ def __set_vars(ds, store, mode='serial'):
     elif mode == 'threads':
         with ProcessPoolExecutor(max_workers=8) as executor:
             for name in dataset.variables.keys():
-                executor.submit(__set_var_t, ds, store, name)
+                executor.submit(__set_var_p, ds, store, name)
     elif mode == 'processes':
         with ThreadPoolExecutor(max_workers=8) as executor:
             for name in dataset.variables.keys():
-                executor.submit(__set_var_p, ds, store, name)
+                executor.submit(__set_var_t, ds, store, name)
     else:
         raise ValueError('the mode %s is not valid.' % mode)
 
@@ -302,14 +313,16 @@ def __configure(args):
         def __call__(self, parser, namespace, values, option_string=None):
             setattr(namespace, self.dest, values.split(' '))
 
-    parser = argparse.ArgumentParser(description=('This script converts netCDF4 into zarr files\n'
-                                                  'It is possible to specify a list of netCDF4 files\n'
-                                                  'to merge them into a single zarr file.\n'
-                                                  'to do so, the variables in the netCD4 file\n'
-                                                  'must have a common shape and run along the same dimension.\n'
-                                                  '\nExamples:\n'
-                                                  '  -convert.py src.nc dst.nc [-options]\n'
-                                                  '  -convert.py "src1.nc src2.nc src3.nc" dst.zarr [-options]\n'),
+    desc = ('This script converts netCDF4 into zarr files\n'
+            'It is possible to specify a list of netCDF4 files\n'
+            'to merge them into a single zarr file.\n'
+            'to do so, the variables in the netCD4 file\n'
+            'must have a common shape and run along the same dimension.\n'
+            '\nExamples:\n'
+            '  -convert.py src.nc dst.nc [-options]\n'
+            '  -convert.py "src1.nc src2.nc src3.nc" dst.zarr [-options]\n')
+
+    parser = argparse.ArgumentParser(description=desc,
                                      formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument('src',
@@ -336,7 +349,7 @@ def __configure(args):
     parser.add_argument('-m',
                         '--mode',
                         metavar='mode',
-                        help='transfer size for multipart',
+                        help='use processes or threads',
                         choices=('threads', 'processes'),
                         default='serial')
 
